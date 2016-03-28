c574a51238c8a52fdf45697fc7bbd87edbf679d2
jeantimex <jean.timex@gmail.com>
2016-03-27 18:28:54 -0700

    Fixed ex7.rb

--- ex7.rb
+++ ex7.rb
@@ -1,4 +1,4 @@
-ry had a little lamb."
+"ruby had a little lamb."
 puts "Its fleece was white as %s." % 'snow'
 puts "And everywhere that Mary went."
 puts "." * 10  # what'd that do?
@@ -21,5 +21,3 @@ end12 = "r"
 print end1 + end2 + end3 + end4 + end5 + end6
 print end7 + end8 + end9 + end10 + end11 + end12
 
-# this just is polite use of the terminal, try removing it
-Puts
